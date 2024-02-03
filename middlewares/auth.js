386dd9be4b73ca17cc4a462048c221578f255749
Khazdev <xazanoff17@gmail.com>
2024-02-03 19:49:59 +0300

    исправил 500 ошибку, если не было токена

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,14 +3,17 @@ const NotAuthorizedError = require('../errors/NotAuthorizedError');
 const config = require('../config');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization.replace('Bearer ', '');
+  const token = req.headers.authorization;
 
-  if (!token) {
+  if (!token || !token.startsWith('Bearer ')) {
     return next(new NotAuthorizedError('Требуется авторизация'));
   }
+
+  const tokenValue = token.replace('Bearer ', '');
+
   let payload;
   try {
-    payload = jwt.verify(token, config.jwtSecret);
+    payload = jwt.verify(tokenValue, config.jwtSecret);
   } catch (e) {
     return next(new NotAuthorizedError('Неверный токен авторизации'));
   }
